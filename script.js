2ffccdd076cefdf42e1b8177a9bd13be744f05b6
Sagar Bisht <itssagar11@gmail.com>
2022-05-01 16:59:01 +0530

    OutSide dimensions Set

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-
 "use strict";
 
 function move(event){
-console.log(event.clientX+" "+event.clientY);
+  console.log(event.clientX+" "+event.clientY);
   let di= this.getBoundingClientRect();
-  let x=(event.clientX-(50/2)-di.left)+'px';
-  let y=(event.clientY-(50/2)-di.top)+'px';
-  if(y<0) y= 0+'px';
-  console.log(x+y+" "+di.left+" "+di.top+" "+di.width+" "+di.height);
-document.getElementById("ball").style.left = x;
-document.getElementById("ball").style.top=y;
+  let x=(event.clientX-(50/2)-di.left);
+  let y=(event.clientY-(50/2)-di.top);
+  if(y<0) y=0;
+  if(x<0) x=0;
+  if(y+di.left+(50/2)>=di.left) y=y-(50/2);
+  if(x+di.top+(50/2)>=di.bottom) x=x-(50/2);
+  x+='px';
+  y+='px';
+  console.log((x+di.left)+y+" "+di.left+" "+di.top);
+  document.getElementById("ball").style.left = x;
+  document.getElementById("ball").style.top=y;
 
 }
 
